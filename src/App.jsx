b0d39e35a687302a45ddc018957d19b96ea24ca0
kelvin Rodriguez <krodriguez6223@gmail.com>
2024-01-28 21:45:03 -0500

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import { useState } from 'react'
 
 function App() {
 
+const sumar = () =>{
+  const valor1 = 25;
+  const valor2 = 255;
+ 
+  return valor1 + valor2;
+}
+
+
   return (
   <div>
-      <h3>Hola mundo</h3>
+      <h3>{'Hola mundo dev'.toUpperCase()} </h3>
+      <p>el valor de la suma es de: {sumar()}</p>
+     
   </div>
   )
 }
